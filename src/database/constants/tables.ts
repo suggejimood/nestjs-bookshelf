7dfb08b919baf1b8faf88c326ce9d29c75b5ef02
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-08-27 11:34:32 +0300

    feat: Table enum added.

---
+++ src/database/constants/tables.ts
@@ -0,0 +1,8 @@
+export enum DatabaseTables {
+  T_Author = 'authors',
+  T_Category = 'categories',
+  T_Book = 'books',
+  //Join tables
+  T_BookAuthors = 'books_authors',
+  T_BooksCategoryies = 'books_categories'
+}
