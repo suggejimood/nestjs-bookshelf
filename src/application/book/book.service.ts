32912d91164fde71ace35eac2cc8577b7c106675
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-08-28 03:13:58 +0300

    feat: Book service created.

--- src/application/book/book.service.ts
+++ src/application/book/book.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PageResult } from 'src/common/pagination';
 import { BookEntity } from 'src/database/entities';
 import { Repository } from 'typeorm';
 
@@ -10,13 +11,80 @@ export class BookService {
     private readonly bookRepository: Repository<BookEntity>,
   ) {}
 
-  public async create(): Promise<any> {}
+  public async create(data: {
+    name: string;
+    isbn?: string;
+    about?: string;
+    quantity: number;
+    authorIds: string[];
+    categoryIds: string[];
+  }): Promise<any> {}
 
-  public async findById(): Promise<any> {}
+  public async findById(data: { id: string }): Promise<BookEntity> {
+    const book = await this.bookRepository.findOne({
+      where: { id: data.id },
+      relations: ['authors', 'categories'],
+    });
 
-  public async update(): Promise<any> {}
+    if (!book) {
+      throw new NotFoundException('Kitap Bulunamadı');
+    }
 
-  public async list(): Promise<any> {}
+    return book;
+  }
 
-  public async delete(): Promise<any> {}
+  public async update(data: {
+    id: string;
+    name?: string;
+    isbn?: string;
+    about?: string;
+    quantity?: number;
+    authorIds?: string[];
+    categoryIds?: string[];
+  }): Promise<any> {}
+
+  public async list(data: {
+    page: number;
+    limit: number;
+    search?: string;
+  }): Promise<
+    PageResult<{
+      id: string;
+      name: string;
+      categories: string[];
+      authors: string[];
+    }>
+  > {
+    const queryBuilder = this.bookRepository
+      .createQueryBuilder('b')
+      .select([
+        `b.id AS id`,
+        `b.name AS name`,
+        `ARRAY_AGG(c.name) AS categories`,
+        `ARRAY_AGG(a.name) AS authors`,
+      ]);
+
+    if (data.search) {
+      queryBuilder.where(`b.name ILIKE :search`, {
+        search: `%${data.search}%`,
+      });
+    }
+
+    const [items, total] = await queryBuilder.getManyAndCount();
+
+    return {
+      items: items as any,
+      limit: data.limit,
+      page: data.page,
+      total: total,
+    };
+  }
+
+  public async deleteById(data: { id: string }): Promise<void> {
+    const result = await this.bookRepository.delete({ id: data.id });
+
+    if (!result.affected) {
+      throw new NotFoundException('Kitap bulunamdı');
+    }
+  }
 }
